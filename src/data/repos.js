6d05b3ae27d36146b2dece8397624be836985228
Andrea Riccetti <andreariccetti@Andreas-MacBook-Pro.local>
2019-04-20 00:17:03 +0200

    m

--- src/data/repos.js
+++ src/data/repos.js
@@ -42,9 +42,3 @@ export const REPOS = [
     color: "#f78fc6"
   }
 ];
-
-// https://github.ibm.com/api/v3/repos/sts-chw-chapter/SHW-kaizen-wow?access_token=d6ce7c2a96a7146f1da65e4392c37d19db1a7a7f
-// https://github.ibm.com/api/v3/repos/STS-EMEA-Kaizen/BPG-flow?access_token=d6ce7c2a96a7146f1da65e4392c37d19db1a7a7f
-// https://github.ibm.com/api/v3/repos/RICCETTI/ES-Prioritization?access_token=d6ce7c2a96a7146f1da65e4392c37d19db1a7a7f
-
-// https://github.ibm.com/api/v3/repos/sts-chw-chapter/SHW-kaizen-wow
